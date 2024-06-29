ccf6cd8ec1081d578f1b5859be85f258e25718b1
Nabin_sharma <105613345+NabinSharmaitgithub@users.noreply.github.com>
2024-06-29 14:13:14 +0545

    Create build.gradle.kts

---
+++ AnimeFlixProvider/build.gradle.kts
@@ -0,0 +1,31 @@
+
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    compileSdk = 31
+
+    defaultConfig {
+        minSdk = 21
+        targetSdk = 31
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            minifyEnabled(false)
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+}
+
+dependencies {
+    implementation(project(":core"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.1")
+    implementation("org.jsoup:jsoup:1.14.3")
+}
